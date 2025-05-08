53807a1784e0b1c70ace9064d00bc396e85979a7
t3nsor27 <srikant.sahoo.144@gmail.com>
2025-05-08 18:28:06 +0530

    fixed iframe and added songname and singer in html

--- MusicMatch/urls.py
+++ MusicMatch/urls.py
@@ -19,6 +19,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/',include('application.urls'))
+    path('play/',include('application.urls'))
 ]
 
--- application/views.py
+++ application/views.py
@@ -20,19 +20,23 @@ def idk(request):
 
 @csrf_exempt
 def upload_audio(request):
+    print("hi")
     if request.method == 'POST' and request.FILES.get('audio'):
         audio_file = request.FILES['audio']
 
         # Save original webm
-        temp_input_path = default_storage.save('temp_input.wav', audio_file)
+        temp_input_path = default_storage.save('temp/temp_input.wav', audio_file)
         input_path = os.path.join(settings.MEDIA_ROOT, temp_input_path)
-        #print("temp:",temp_input_path, "\ninp:",input_path)
+        print("temp:",temp_input_path, "\ninp:",input_path)
 
         # DO PROCESSING 
-
+        json_out = {'status': 'success'}
+        # UNCOMMENT THIS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # json_out["song"] = find_song(temp_input_path)
         # Clean up temp file
         # default_storage.delete(temp_input_path)
+        print(json_out)
 
-        return JsonResponse({'status': 'success'}) #SEND TO FRONTEND
-
-    return JsonResponse({'status': 'error', 'message': 'No audio file uploaded'})
+        # example output comment this !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        json_out = {'status': 'success', 'song': {'song_id': 0, 'song_file': './media/songs/song4.wav', 'singer': 'me', 'song_name': ' Force Projection | Cyberpunk 2077', 'link': 'bSpQpImpZbw', 'time': 54}}
+        return JsonResponse(json_out) #SEND TO FRONTEND
